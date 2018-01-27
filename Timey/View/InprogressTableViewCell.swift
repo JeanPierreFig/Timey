d2cb4e0ca356de05c193ad4adadce27453fc37e9
Jean Pierre <jfigaredomoro@fullsail.edu>
2018-01-27 13:37:18 -0500

    Refactored the CameraViewController

--- Timey/View/InprogressTableViewCell.swift
+++ Timey/View/InprogressTableViewCell.swift
@@ -1,18 +1,26 @@
 //
-//  TableViewCell.swift
+//  InprogressTableViewCell.swift
 //  Timey
 //
-//  Created by Jean Pierre on 1/10/18.
+//  Created by Jean Pierre on 1/25/18.
 //  Copyright © 2018 Jean Pierre Figaredo. All rights reserved.
 //
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
 
+class InprogressTableViewCell: UITableViewCell {
+
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var title:UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        photoView.layer.cornerRadius = 6
+        photoView.layer.borderWidth = 1
+        photoView.layer.borderColor = UIColor.lightGray.cgColor
+        photoView.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
